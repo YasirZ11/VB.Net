5e8aebbc3e7a432e94498155e2c919e23ac98882
Mohamed Zulfikar Yasir <112502490+YasirZ11@users.noreply.github.com>
2024-05-01 15:24:00 +0530

    Add files via upload

---
+++ Input/Input/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim m As Integer
+
+        m = InputBox("Enter a number", "Value")
+
+        If m > 50 Then
+            MsgBox("The number is greater than 50")
+
+        ElseIf m = 50 Then
+            MsgBox("The number is equal to 50")
+
+        Else
+            MsgBox("The number is less than50")
+        End If
+    End Sub
+End Class
---
+++ InterEx2/InterEx2/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label2.Text = " Button--1 Clicked"
+
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Label2.Text = " Button--2 Clicked"
+    End Sub
+
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+End Class
